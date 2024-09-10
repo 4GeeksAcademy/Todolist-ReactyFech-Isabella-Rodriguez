a53d241ab2fa4d6d87222fc9c1742fd433ef9753
Isabella-Rodriguez <75060286+Isabella-Rodriguez@users.noreply.github.com>
2024-09-10 18:27:23 +0000

    todolist w react and fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [input, setInput] = useState("");
+  const [task, setTask] = useState([]);
+
+  function saveTodos() {
+    fetch("https://playground.4geeks.com/todo/users/isabella")
+      .then((response) => response.json())
+      .then((data) => setTask(data.todos));
+  }
+
+  function addTodos(inputValue) {
+    fetch("https://playground.4geeks.com/todo/todos/isabella", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        label: inputValue,
+        is_done: false,
+      }),
+    })
+      .then((response) => response.json())
+      .then(() => saveTodos());
+  }
+
+  function deleteTodos(selectId) {
+    fetch("https://playground.4geeks.com/todo/todos/" + selectId, {
+      method: "DELETE",
+      redirect: "follow",
+    })
+      .then(() => saveTodos())
+      .catch((error) => console.error(error));
+  }
+
+  // Nueva función para eliminar todas las tareas
+  function deleteAllTodos() {
+    task.forEach((t) => {
+      fetch("https://playground.4geeks.com/todo/todos/" + t.id, {
+        method: "DELETE",
+        redirect: "follow",
+      })
+        .then(() => saveTodos())
+        .catch((error) => console.error(error));
+    });
+  }
+
+  useEffect(() => {
+    saveTodos();
+  }, []);
+
+  return (
+    <div className="todo-container">
+      <h1>Todos</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              addTodos(input);
+              setInput("");
+            }
+          }}
+        />
+      </div>
+      <ul className="task-list">
+        {task.map((t, index) => (
+          <li key={index} className="task-item">
+            <span>{t.label}</span>
+            <i
+              className="fa fa-times remove-task"
+              aria-hidden="true"
+              onClick={() => deleteTodos(t.id)}
+            ></i>
+          </li>
+        ))}
+        {task.length === 0 && <li className="no-tasks">No tasks, add a task</li>}
+      </ul>
+
+      {/* Botón para eliminar todas las tareas */}
+      <div className="delete-all-button">
+        <button onClick={deleteAllTodos} className="btn btn-danger">
+          Eliminar todas las tareas
+        </button>
+      </div>
+      <div className="tasks-left">
+        {task.length === 1
+          ? `${task.length} task left`
+          : `${task.length} tasks left`}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
